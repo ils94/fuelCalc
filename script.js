705c5b122fd44b646db79f8e7905d73f952bae8a
ILS <99141005+ils94@users.noreply.github.com>
2025-01-10 16:48:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -21,22 +21,17 @@ function calculateCost() {
     const totalGasolineCost = gasolineCostPerKm * distance;
     const totalAlcoholCost = alcoholCostPerKm * distance;
 
-    // Aplica a regra dos 75%
-    const alcoholIsViableByEfficiency = alcoholCostPerKm <= 0.75 * gasolineCostPerKm;
+    const efficiency = alcoholKm / gasolineKm;
+
+    const alcoholIsViableByEfficiency = alcoholPrice <= efficiency * gasolinePrice;
 
     // Determina a melhor opção
     let cheaperOption = "";
 
     if (alcoholIsViableByEfficiency) {
-        cheaperOption = `
-        Álcool é melhor levando em consideração a
-        eficiência do consumo e a regra dos 75%.
-        `;
+        cheaperOption = `O etanol é a opção mais vantajosa, considerando seu preço e o rendimento de ${efficiency*100}% que este carro alcança com seu uso.`;
     } else {
-        cheaperOption = `
-        Gasolina é melhor levando em consideração a
-        eficiência do consumo e a regra dos 75%.
-        `;
+        cheaperOption = `A gasolina é a opção mais vantajosa, considerando seu preço e o rendimento de ${efficiency*100}% que este carro alcança com o uso do etanol.`;
     }
 
     // Exibe os resultados
@@ -47,7 +42,7 @@ function calculateCost() {
     <div class="cost">Custo por KM: R\$ ${gasolineCostPerKm.toFixed(2)}<br>Total: R\$ ${totalGasolineCost.toFixed(2)}</div>
     </div>
     <div class="fuel-type${alcoholIsViableByEfficiency ? " highlight" : ""}">
-    <div class="name">Álcool:</div>
+    <div class="name">Etanol:</div>
     <div class="cost">Custo por KM: R\$ ${alcoholCostPerKm.toFixed(2)}<br>Total: R\$ ${totalAlcoholCost.toFixed(2)}</div>
     </div>
     <div class="message">${cheaperOption}</div>
